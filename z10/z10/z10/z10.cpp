5b954eff8b7a9377fc878cd3bd972659934a473b
przemek768 <przemek76816@gmail.com>
2021-01-03 15:50:37 +0100

    Zadanai testowe

---
+++ z10/z10/z10/z10.cpp
@@ -0,0 +1,33 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int minParzysta(int tablica[], int rozmiar) {
+	int small = tablica[0];
+	int min = 0;
+	for (int i = 0; i < rozmiar; i++) {
+		if (tablica[i] % 2 == 0 && tablica[i] < small) {
+			if (tablica[i] < small) {
+				small = tablica[i];
+				min = i;
+			}
+		}
+	}
+	tablica[small] = min;
+	return small;
+}
+
+int main()
+{
+	const int rozmiar = 10;
+	int tablica[10] = {1,2,3,4,5,6,7,8,9,1};
+	//for (int i = 0; i < rozmiar; i++) {
+		//tablica[i] = rand() % rozmiar;
+		//cout << tablica[i];
+	//}
+
+	cout << "\n";
+	cout << minParzysta(tablica, rozmiar);
+	return 0;
+}
---
+++ z12/z12/z12/z12.cpp
@@ -0,0 +1,24 @@
+﻿
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int f1(char napis[]) {
+	int wyn = 0;
+	for (int i = 0; i < strlen(napis); i++) {
+		if (napis[i] != napis[i]) {
+			wyn = napis[i];
+		}
+	}
+	return wyn;
+}
+
+int main()
+{
+	cout << "Podaj napis \n";
+	char napis[20];
+	cin.get(napis, 20);
+	cout << f1(napis);
+	return 0;
+}
